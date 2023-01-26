882f11a487c7f120a50e7df4a94e76e1efa94f83
Soly Soly <ssoly2152@gmail.com>
2023-01-26 22:27:42 +0200

    private SqlConnection Con; private SqlCommand Cmd;

--- EmployeeMgmt1/funcbona.cs
+++ EmployeeMgmt1/funcbona.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,9 @@ using System.Threading.Tasks;
 namespace EmployeeMgmt1
 {
     internal class funcbona
-    {udi
+    {
+        private SqlConnection Con;
+        private SqlCommand Cmd;
+        private DataTable dt;
     }
 }
