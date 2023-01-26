495745b6e91e469101be36db12b8ab044727b350
Soly Soly <ssoly2152@gmail.com>
2023-01-27 00:43:37 +0200

    login Obj  InitializeComponent

--- EmployeeMgmt1/Form4.cs
+++ EmployeeMgmt1/Form4.cs
@@ -7,7 +7,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-Update DepartmentTb1 set Depname
+Update DepartmentTb1 set Depnamelogin Obj
 using System.Windows.Forms;
 
 namespace EmployeeMgmt1
